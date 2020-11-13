c83b2b3d3be0a41cd50b4130ff1dec917357ac6a
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2020-11-13 22:03:59 +0530

    Add files via upload

---
+++ Assignment_2/Program_1/Header.h
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+class Display
+{
+    private:
+        int iNo;
+    
+    public:
+        Display();
+        Display(int iNo);
+        void DisplayPattern();
+};
